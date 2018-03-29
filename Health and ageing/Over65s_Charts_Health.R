ce144519bebf2299806bdd723d7b8ffc97583ab2
zmek <zella@personalboardroom.com>
2018-03-29 18:50:14 +0100

    New project directory

--- Health and ageing/Over65s_Charts_Health.R
+++ Health and ageing/Over65s_Charts_Health.R
@@ -115,10 +115,7 @@ dev.off()
 ### --- circular barplot for raw pop numbers - combining limited and a lot - by district --- ###
 
 #make dataframe with raw pop numbers 
-e<-copy(health_onlytot)
-e<-e[ county_level==0]
-e<-e[, y2017:Limited]
-e<-e[ , .(y2021 = sum(y2021)), by = .(district)]
+e<-copy(health_onlytot_nocounty_nolimited)
 e<-e[order(-y2021)] #order by relevant field - do this every time
 plotdata=data.frame( 
         id<-seq(1,nrow(e)),
@@ -136,12 +133,13 @@ label_data$hjust<-ifelse( angle < -90, 1, 0)
 label_data$angle<-ifelse(angle < -90, angle+180, angle)
 
 #add new CAGR labels to the label data
-label_data$district<-as.factor(paste(as.character(plotdata$district)," (",round(plotdata$y2021_orig/1000,1),"K)",sep=""))
+label_data$district<-as.factor(paste(as.character(plotdata$district)," (",round(plotdata$y2021_orig/1000,1),")",sep=""))
 
 #print to tiff file
-tiff('District_healthtotals.tiff', units="in", width=5, height=5, res=100, compression = 'lzw')
+tiff('District_healthtotals.tiff', units="in", width=6, height=6, res=100, compression = 'lzw')
 p = ggplot(plotdata, aes(x=as.factor(id), y=y2021, fill = topten)) +                                         geom_bar(stat="identity") + ylim(-100,200) + 
         theme_minimal() +
+        scale_fill_manual(values = c("1" = "blue", "0" = "orange")) +
         theme(axis.text = element_blank(), axis.title = element_blank(), panel.grid = element_blank(), plot.margin = unit(rep(-1,4), "cm")) +
         coord_polar(start = 0) +
         theme(legend.position = "none") +
@@ -149,3 +147,37 @@ p = ggplot(plotdata, aes(x=as.factor(id), y=y2021, fill = topten)) +            
 p
 dev.off()
 
+
+
+### --- circular barplot for CAGR2025 by district - combining limited and a lot - --- ###
+
+#edit dataset for plotting CAGR - NB without changing order for this plot
+plotdata$cagr2025_orig<-e$cagr2025
+plotdata$cagr2025<-e$cagr2025/max(e$cagr2025)*100
+colnames(plotdata)<-c("id","district","y2021_orig","y2021","topten","cagr2025_orig","cagr2025")
+
+#to calc top ten cagr without disturbing ordering of this data table
+temp<-copy(e)
+temp<-temp[order(-cagr2025)] #first reorder temp DT in order of CAGR
+temp<-temp[,toptenCAGR2025:=factor(c(rep(0,10),rep(1,nrow(b)-10)))] #create new top ten factor in the correct order
+temp<-temp[order(-y2021)] #put back into original order
+
+#write to plot data
+plotdata$toptenCAGR<-temp$toptenCAGR2025
+rm(temp)
+
+#add new CAGR labels to the label data
+label_data$district<-as.factor(paste(as.character(plotdata$district)," (",round(plotdata$cagr2025_orig*100,1),"%)",sep=""))
+cagr2025<-plotdata$cagr2025 #?? not sure why this plot which is same as above doesn't work without this
+
+#print to tiff file
+tiff('District_healthCAGR2025.tiff', units="in", width=6, height=6, res=100, compression = 'lzw')
+p = ggplot(plotdata, aes(x=as.factor(id), y=cagr2025, fill = toptenCAGR)) +                                         geom_bar(stat="identity") + ylim(-100,200) + 
+        theme_minimal() +
+        scale_fill_manual(values = c("1" = "blue", "0" = "orange")) +
+        theme(axis.text = element_blank(), axis.title = element_blank(), panel.grid = element_blank(), plot.margin = unit(rep(-1,4), "cm")) +
+        coord_polar(start = 0) +
+        theme(legend.position = "none") +
+        geom_text(data=label_data, aes(x=id, y=cagr2025+10, label=district, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) 
+p
+dev.off()
